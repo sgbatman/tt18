cd701e64002812c94c32d6d83b6dbe001d5b5a96
sgbatman <42965459+sgbatman@users.noreply.github.com>
2021-11-11 16:30:46 +0800

    fromat codes

--- react_fe/src/App.js
+++ react_fe/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 // import logo from "./dbs-logo2.png";
 import "./App.css";
-import Login from "./Component/login"
+import Login from "./Component/login";
 import AddExpenses from "./Component/AddExpenses";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Login displaytext="Login Screen" />
 
-        <Login displaytext="Login Screen"/>
+        <br />
+        <br />
         <AddExpenses />
       </div>
-);
+    );
   }
 }
 export default App;
