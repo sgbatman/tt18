c95bf01c17b694ae149f709524bbb74f61e2850f
Stephanie De Cotta <Stephanie@MacBook-Air-5.local>
2021-11-11 15:09:33 +0800

    Login Refactored

--- react_fe/src/App.js
+++ react_fe/src/App.js
@@ -1,56 +1,15 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
+// import logo from "./dbs-logo2.png";
 import "./App.css";
+import Login from "./Component/login"
 
 class App extends Component {
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log(e.target.email.value);
-
-    if (!e.target.email.value) {
-      alert("Email is required");
-    } else if (!e.target.email.value) {
-      alert("Valid email is required");
-    } else if (!e.target.password.value) {
-      alert("Password is required");
-    } else if (
-      e.target.email.value === "me@example.com" &&
-      e.target.password.value === "123456"
-    ) {
-      alert("Successfully logged in");
-      e.target.email.value = "";
-      e.target.password.value = "";
-    } else {
-      alert("Wrong email or password combination");
-    }
-  };
-
-  handleClick = e => {
-    e.preventDefault();
-
-    alert("Goes to registration page");
-  };
-
   render() {
     return (
       <div className="App">
-        <form className="form" onSubmit={this.handleSubmit}>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="name@email.com" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
-          </div>
-          <button className="primary">Enter</button>
-        </form>
-        <button className="secondary" onClick={this.handleClick}>
-          Create an Account
-        </button>
+        <Login displaytext="Login Screen"/>
       </div>
-    );
+);
   }
 }
-
 export default App;
